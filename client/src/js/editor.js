dc359ee29b6cc966cfb231481321d6129bf9a480
C M Santosh Kumar <pagadala.kalyani@gmail.com>
2023-03-30 21:50:00 +0100

    pushing heroku

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -22,7 +22,7 @@ export default class {
       tabSize: 2,
     });
 
-    // When the editor is ready, set the value to whatever is stored in indexeddb.
+
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
       console.info('Loaded data from IndexedDB, injecting into editor');
